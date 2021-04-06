a46c3f8679d07681c9112d2d4c4c5ce59e72b9a6
= <=>
2021-04-06 12:11:09 +0300

    added fizzbuzz code for js & rs

---
+++ fizzbuzz.rs
@@ -0,0 +1,14 @@
+fn main() {
+
+    for x in 1..101 {
+        if x % 15 == 0 {
+            println!("FizzBuzz");
+        }else if x % 5 == 0 {
+            println!("Buzz");
+        }else if x % 3 == 0 {
+            println!("Fizz");
+        } else {
+            println!("{}", x);
+        }     
+    }
+}
